d3ef05bffaaa71cc539ee53584be850b0813c04d
Icaro Dell Osso <icaro.dellosso@hotmail.com>
2022-11-21 11:39:53 -0300

    novos formulários

--- atvii-wb-typescript-master/src/componentes/formulários/formularioCadastroCliente.tsx
+++ atvii-wb-typescript-master/src/componentes/formulários/formularioCadastroCliente.tsx
@@ -1,45 +1,110 @@
 import { Component } from "react";
 
 type props = {
-    tema: string
-}
+  tema: string;
+};
 
 export default class FormularioCadastroCliente extends Component<props> {
-    
-    render() {
-        let estiloBotao = `btn waves-effect waves-light ${this.props.tema}`
-        return (
-            <div className="row">
-                <form className="col s12">
-                    <div className="row">
-                        <div className="input-field col s6">
-                            <input id="first_name" type="text" className="validate" />
-                            <label htmlFor="first_name">nome</label>
-                        </div>
-                        <div className="input-field col s6">
-                            <input id="last_name" type="text" className="validate" />
-                            <label htmlFor="last_name">sobrenome</label>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="input-field col s6">
-                            <input id="telefone" type="text" className="validate" />
-                            <label htmlFor="telefone">telefone</label>
-                        </div>
-                        <div className="input-field col s6">
-                            <input id="email" type="email" className="validate" />
-                            <label htmlFor="email">e-mail</label>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col s12">
-                            <button className={estiloBotao} type="submit" name="action">Submit
-                                <i className="material-icons right">send</i>
-                            </button>
-                        </div>
-                    </div>
-                </form>
+  componentDidMount(): void {
+    var elems = document.querySelectorAll("select");
+    M.FormSelect.init(elems);
+  }
+
+  render() {
+    let estiloBotao = `btn waves-effect waves-light ${this.props.tema}`;
+    return (
+      <div className="row">
+        <form className="col s12">
+          <div id="modalLine" className="row">
+            <div className="input-field col s7">
+              <input id="first_name" type="text" className="validate" />
+              <label htmlFor="first_name">nome</label>
             </div>
-        )
-    }
+            <div className="input-field col s5">
+              <input id="last_name" type="text" className="validate" />
+              <label htmlFor="last_name">sobrenome</label>
+            </div>
+          </div>
+          <div id="modalLine" className="row">
+            <div className="input-field col s12">
+              <input id="socialName" type="text" className="validate" />
+              <label htmlFor="socialName">Nome Social</label>
+            </div>
+          </div>
+          <div id="modalLine" className="row">
+            <div className="input-field col s5">
+              <select>
+                <option value="">Escolha uma opção</option>
+                <option value="1">Feminino</option>
+                <option value="2">Masculino</option>
+                <option value="3">Outro</option>
+              </select>
+              <label htmlFor="gender">Sexo</label>
+            </div>
+            <div className="input-field col s3">
+              <input id="BirthdayDate" type="date" className="validate" />
+              <label htmlFor="BirthdayDate">Data de Nascimento</label>
+            </div>
+            <div className="input-field col s4">
+              <input id="CPF" type="text" className="validate" />
+              <label htmlFor="CPF">CPF</label>
+            </div>
+          </div>
+          <div id="modalLine" className="row">
+            <div className="input-field col s7">
+              <input id="email" type="email" className="validate" />
+              <label htmlFor="email">e-mail</label>
+            </div>
+            <div className="input-field col s5">
+              <input id="telefone" type="text" className="validate" />
+              <label htmlFor="telefone">Telefone</label>
+            </div>
+          </div>
+          <h6>Endereço</h6>
+          <div id="modalLine" className="row">
+            <div className="input-field col s7">
+              <input id="cidade" type="text" className="validate" />
+              <label htmlFor="cidade">Cidade</label>
+            </div>
+            <div className="input-field col s2">
+              <input id="estado" type="text" className="validate" />
+              <label htmlFor="estado">Estado</label>
+            </div>
+            <div className="input-field col s3">
+              <input id="cep" type="text" className="validate" />
+              <label htmlFor="cep">CEP</label>
+            </div>
+          </div>
+          <div id="modalLine" className="row">
+            <div className="input-field col s7">
+              <input id="rua" type="text" className="validate" />
+              <label htmlFor="rua">Rua</label>
+            </div>
+            <div className="input-field col s1">
+              <input id="numero" type="text" className="validate" />
+              <label htmlFor="numero">Nº</label>
+            </div>
+            <div className="input-field col s4">
+              <input id="complemento" type="text" className="validate" />
+              <label htmlFor="complemento">Complemento</label>
+            </div>
+          </div>
+          <div id="modalLine" className="row">
+            <div className="input-field col s4">
+              <input id="bairro" type="text" className="validate" />
+              <label htmlFor="bairro">Bairro</label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col s12">
+              <button className={estiloBotao} type="submit" name="action">
+                Submit
+                <i className="material-icons right">send</i>
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
